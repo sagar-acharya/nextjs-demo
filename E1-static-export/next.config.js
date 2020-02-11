d236f447e3b21f729cd6069c485a585c353715a8
Sagar Acharya <sagar.acharya@weboniselab.com>
2020-02-11 14:08:33 +0530

    lazy loading

---
+++ E1-static-export/next.config.js
@@ -0,0 +1,21 @@
+const fetch = require('isomorphic-unfetch');
+
+module.exports = {
+  exportTrailingSlash: true,
+  exportPathMap: async function() {
+    const paths = {
+      '/': { page: '/' },
+      '/about': { page: '/about' },
+      '/show/975': { page: '/show/[id]', query: { id: '975' } }
+    };
+    const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+    const data = await res.json();
+    const shows = data.map(entry => entry.show);
+
+    shows.forEach(show => {
+      paths[`/show/${show.id}`] = { page: '/show/[id]', query: { id: show.id } };
+    });
+
+    return paths;
+  }
+};
--- E2-lazy-loading-modules/lib/load-db.js
+++ E2-lazy-loading-modules/lib/load-db.js
@@ -1,6 +1,6 @@
 export default async function loadDb() {
-  const firebase = require('firebase/app')
-  require('firebase/database')
+  const firebase = await import('firebase/app');
+  await import('firebase/database');
 
   try {
     firebase.initializeApp({
